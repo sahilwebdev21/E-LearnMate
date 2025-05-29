a290ea289d931d9c99737c09b00342628db38b3e
sahilwebdev21 <sahilwebdev21@gmail.com>
2025-05-29 21:28:59 +0545

    Initial project structure and Next.js/Django setup

---
+++ fronted/src/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen">
+      <h1 className="text-4xl font-bold text-gray-800">E-LearnMate</h1>
+      <p className="mt-4 text-lg text-gray-600">Coming Soon</p>
+    </main>
+  );
+}
